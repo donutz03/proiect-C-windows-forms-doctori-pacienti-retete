f8bc35901319643b84e60450a630ab040e6bcef5
donutz03 <109514731+donutz03@users.noreply.github.com>
2024-06-07 10:30:54 +0300

    Update Constants.cs

--- Constants.cs
+++ Constants.cs
@@ -2,6 +2,6 @@
 {
     public static class Constants
     {
-        public const string ConnectionString = "Data Source=(DESCRIPTION=(ADDRESS_LIST=(ADDRESS=(PROTOCOL=TCP)(HOST=193.226.34.57)(PORT=1521)))(CONNECT_DATA=(SERVER=DEDICATED)(SERVICE_NAME=orclpdb.docker.internal)));User Id=HODOROAGAI_56;Password=stud;";
+        public const string ConnectionString = "Data Source=(DESCRIPTION=(ADDRESS_LIST=(ADDRESS=(PROTOCOL=TCP)(HOST=193.226.34.57)(PORT=1521)))(CONNECT_DATA=(SERVER=DEDICATED)(SERVICE_NAME=orclpdb.docker.internal)));User Id=;Password=;";
     }
 }
